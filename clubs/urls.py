eb7aff702d5f128bde850eade84a7b15fc754c24
HelloOjasMutreja <helloojasmutreja@gmail.com>
2025-08-18 22:17:22 +0530

    setting up a base structure for club model

---
+++ clubs/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('', views.listClubs, name='list_clubs'),
+    # path('create/', views.createClub, name='create_club'),
+    # path('update/<int:club_id>/', views.updateClub, name='update_club'),
+    # path('delete/<int:club_id>/', views.deleteClub, name='delete_club'),
+    # path('view/<int:club_id>/', views.viewClub, name='view_club'),
+]
--- clubs/views.py
+++ clubs/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# def createClub(request):
+
+# def updateClub(request, club_id):
+    # edit admins
+
+# def deleteClub(request, club_id):
+
+# def viewClub(request, club_id):
+
+# def listClubs(request):
+
