5dee4ae41ca1e9e2294c34df6b52e6af46eafb49
HelloOjasMutreja <helloojasmutreja@gmail.com>
2025-08-26 20:04:55 +0530

    setting up CustomUser model in accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class CustomUser(models.Model):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    current_year_of_study = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
