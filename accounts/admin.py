b195b6faaa3af8e4ccbab75fe49edb0ac8e45382
HelloOjasMutreja <helloojasmutreja@gmail.com>
2025-08-29 00:37:53 +0530

    bug fixes

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,26 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    list_display = ("email", "first_name", "last_name", "is_staff", "is_active")
-    list_filter = ("is_staff", "is_active", "belongs_to_club")
-    search_fields = ("email", "first_name", "last_name")
-    ordering = ("email",)
 
-    fieldsets = (
-        (None, {"fields": ("email", "password")}),
-        ("Personal info", {"fields": ("first_name", "last_name", "current_year_of_study", "belongs_to_club")}),
-        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
-        ("Important dates", {"fields": ("last_login", "date_joined")}),
-    )
-
-    add_fieldsets = (
-        (None, {
-            "classes": ("wide",),
-            "fields": ("email", "first_name", "last_name", "current_year_of_study", "password1", "password2", "is_staff", "is_active"),
-        }),
-    )
-
-admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(CustomUser)
--- accounts/models.py
+++ accounts/models.py
@@ -1,54 +1,22 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
-from django.utils import timezone
 
+# Create your models here.
 
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, first_name, last_name, current_year_of_study, password=None, **extra_fields):
-        if not email:
-            raise ValueError("The Email field must be set")
-        email = self.normalize_email(email)
-        user = self.model(
-            email=email,
-            first_name=first_name,
-            last_name=last_name,
-            current_year_of_study=current_year_of_study,
-            **extra_fields
-        )
-        user.set_password(password)  # handles hashing
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, first_name, last_name, current_year_of_study, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-
-        return self.create_user(email, first_name, last_name, current_year_of_study, password, **extra_fields)
-
-
-class CustomUser(AbstractBaseUser, PermissionsMixin):
+class CustomUser(models.Model):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    date_joined = models.DateTimeField(default=timezone.now)
+    date_joined = models.DateTimeField(auto_now_add=True)
     current_year_of_study = models.IntegerField()
     belongs_to_club = models.ManyToManyField('clubs.Club', blank=True, related_name='members')
     # profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)  # Optional field for profile pictures
     # role_per_club (to identify domain they are part of) (should be many to many in each club) such as ALex is a "Technical" member and "Core" member in the Club A and is "Creative" member in Club B (can be implemented later if needed)
-
-    # Required fields for auth system
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-
-    objects = CustomUserManager()
-
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["first_name", "last_name", "current_year_of_study"]
+ 
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+    
+class UserProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name="profile")
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"Profile of {self.user.username}"
